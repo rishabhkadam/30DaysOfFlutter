19a8b1bc8ffdb0dd67cce28da99688224ff03e6e
rishabhkadam <rishabhkadam2001@gmail.com>
2022-05-13 18:10:39 +0530

    compeleted day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,27 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:testfile/loginPage.dart';
+import 'package:testfile/routes.dart';
 
 void main() {
-  runApp(const testApp());
+  runApp(MyApp());
 }
 
-// ignore: camel_case_types
-class testApp extends StatefulWidget {
-  const testApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
 
   @override
-  _testAppState createState() => _testAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-// ignore: camel_case_types
-class _testAppState extends State<testApp> {
-  String ButtonName = 'Press';
-  int currentIndex = 0;
-
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Test App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Test File'),
-          backgroundColor: Colors.teal,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  setState(() {
-                    ButtonName = 'ho gya.';
-                  });
-                  // ignore: avoid_print
-                },
-                icon: const Icon(Icons.help_outline),
-                label: Text(ButtonName),
-              )
-            ],
-          ),
-          
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.teal,
-          items: const [
-            BottomNavigationBarItem(
-              label: "Home",
-              icon: Icon(Icons.home),
-            ),
-            BottomNavigationBarItem(
-                label: "Setting", icon: Icon(Icons.settings)),
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
-      ),
+      home:const Text('This is home page.'),
+      initialRoute: '/LoginPage',
+      routes: {'/LoginPage': (context) => const loginPage(),
+      "/home": (context) =>  MyApp()
+      },
     );
   }
 }
